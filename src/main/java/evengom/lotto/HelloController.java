dbf3bce6e62fbdbe52916dfe4644cf0b3c0129a2
Jeewoo Jung <cheltus11@gmail.com>
2022-07-21 16:48:06 +0900

    react-boot 연동

---
+++ src/main/java/evengom/lotto/HelloController.java
@@ -0,0 +1,13 @@
+package evengom.lotto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello(){
+        return  "hello boot-react";
+    }
+}
